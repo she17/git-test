f765083aee252ba26d3b9bd6b861f0bb526dbd84
shero <shero@ngls.com>
2022-11-30 00:06:53 +0800

    hot-fix 2

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -10,7 +10,7 @@ public class GItTest {
         System.out.println("hello git3!");
         System.out.println("hello 4!");
         System.out.println("master test!");
-        System.out.println("hot-fix test!");
+        System.out.println("hot-fixTest!");
         System.out.println("push test!");
         System.out.println("pull test!");
         System.out.println("gitee test!");
